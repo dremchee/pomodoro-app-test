903a83a8d04c67b96a7329bcff012c9eecda7234
dremchee <dremchee@gmail.com>
2024-07-29 13:27:28 +0300

    cleanup

--- src/components/dataForExport/localStorageHelper.ts
+++
@@ -1,8 +0,0 @@
-export function saveToLocalStorage(key: string, value: any) {
-  localStorage.setItem(key, JSON.stringify(value));
-}
-
-export function loadFromLocalStorage(key: string) {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : null;
-}
